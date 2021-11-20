9d83eaabd8f0ea5c16897c054ce5320c3b1ebe22
Anna Krendeleva <krendelevaanna@mail.ru>
2021-11-20 14:10:43 +0300

    fix bugs

--- task1.rb
+++ task1.rb
@@ -1,46 +1,26 @@
 class Station
 
-  attr_reader :list_trains
+  attr_reader :trains
+  attr_reader :name
   def initialize(name)
     @name = name
-    @list_trains = []
+    @trains = []
   end
 
-  def new_train(train)
-    @list_trains << train
-    train.new_station(self)
+  def add_train(train)
+    @trains << train
   end
 
-  def current_trains
-    puts @list_trains
-  end
-
-  def list_trains_by_type(type)
-    new_list = []
-    for train in @list_trains
-      if train.get_type() == type
-        new_list << train
-      end
-    end
-    return new_list
+  def trains_by_type(type)
+    @trains.select{|train| train.type == type}
   end
 
   def count_trains_by_type(type)
-    count = 0
-    for train in @list_trains
-      if train.get_type() == type
-        count += 1
-      end
-    end
-    return count
+    self.trains_by_type(type).size
   end
 
   def depart_train(train)
-    @list_trains.delete_if {|x| x==train}
-  end
-
-  def get_name
-    @name
+    @trains.delete(train)
   end
 
 end
@@ -48,6 +28,9 @@ end
 
 class Train
   
+  attr_reader :size
+  attr_reader :station
+  attr_reader :type
   def initialize(number, type, size)
     @number = number
     @type = type
@@ -67,84 +50,54 @@ class Train
     @speed = 0
   end
 
-  def get_size
-    @size
-  end
-
-  def get_type
-    @type
-  end
-
   def size_increase
-    if @speed == 0
-      @size = @size + 1
-    end
-    return @size
+    @size += 1 if @speed == 0
   end
   
   def size_decrease
-    if @speed == 0
-      @size = @size - 1
-    end
+    @size -= 1 if @speed == 0
     return @size
   end
 
-  def post_route(route)
+  def add_route(route)
     @route = route
-    @station.depart_train(self)
-    @station = route.get_first_station()
-    @station.new_train(self)
+    @station.depart_train(self) if @station != nil
+    @station = route.first_station()
+    @station.add_train(self)
   end
 
-  def new_station(station)
+  def add_station(station)
     @station = station
   end
 
-  def get_station
-    return @station
+  def next_station
+    list = @route.list_stations
+    if @station == list[-1]
+      return @station
+    else
+      return list[list.index { |x| x == @station } + 1]
+    end
   end
 
-  def get_next_station
-    list = @route.get_list_stations
-    for i in (0...list.length-1) do
-      if list[i] == @station
-        return list[i+1]
-      end
+  def previous_station
+    list = @route.list_stations
+    if @station == list[0]
+      return @station
+    else
+      return list[list.index { |x| x == @station} - 1]
     end
-    return false
-  end
-
-  def get_prev_station
-    list = @route.get_list_stations
-    for i in (1...list.length) do
-      if list[i] == @station
-        return list[i-1]
-      end
-    end
-    return false
   end
 
   def go_forward
-    list = @route.get_list_stations
-    for i in (0...list.length-1) do
-      if list[i] == @station
-        @station.depart_train(self)
-        list[i+1].new_train(self)
-        break
-      end
-    end
+    @station.depart_train(self)
+    @station = self.next_station
+    @station.add_train(self)
   end
 
   def go_back
-    list = @route.get_list_stations
-    for i in (1...list.length) do
-      if list[i] == @station
-        @station.depart_train(self)
-        @station = list[i-1]
-        @station.new_train(self)
-        break
-      end
-    end
+    @station.depart_train(self)
+    @station = self.previous_station
+    @station.add_train(self)
   end
 
 end
@@ -163,22 +116,16 @@ class Route
     @list_stations << station
   end
 
-  def get_first_station
+  def first_station
     @first_station
   end
 
-  def get_list_stations
-    new_list = []
-    new_list << @first_station
-    for station in @list_stations
-      new_list << station
-    end
-    new_list << @last_station
-    return new_list
+  def list_stations
+    [@first_station, @list_stations, @last_station].flatten
   end
   
   def delete_station(station)
-    @list_stations.delete_if {|x| x==station}
+    @list_stations.delete {|x| x==station}
   end
 
 end
--- test.rb
+++ test.rb
@@ -11,10 +11,21 @@ s1 = Station.new("Simferopol")
 s2 = Station.new("Moscow")
 s3 = Station.new("Spb")
 
-s1.new_train(t1)
-s1.new_train(t4)
-s2.new_train(t2)
-s3.new_train(t3)
+r1 = Route.new(s3, s1)
+r1.add_station(s2)
+t3.add_route(r1)
+
+r2 = Route.new(s1, s3)
+r1.add_station(s2)
+t1.add_route(r2)
+
+r3 = Route.new(s1, s2)
+r1.add_station(s3)
+t4.add_route(r3)
+
+r4 = Route.new(s2, s3)
+r1.add_station(s1)
+t2.add_route(r4)
 
 assert_equal t1.get_speed(), 0
 t1.speed_increase(30)
@@ -22,13 +33,15 @@ assert_equal t1.get_speed(), 30
 t1.stop()
 assert_equal t1.get_speed(), 0
 
-assert_equal t1.get_size, 4
-assert_equal t1.size_increase, 5
+assert_equal t1.size, 4
+t1.size_increase
+assert_equal t1.size, 5
 t1.speed_increase(30)
-assert_equal t1.size_increase, 5
+t1.size_increase
+assert_equal t1.size, 5
 t1.stop
-assert_equal t1.size_increase, 6
-
+t1.size_increase
+assert_equal t1.size, 6
 
 assert_equal s1.count_trains_by_type("a"), 1
 assert_equal s2.count_trains_by_type("a"), 1
@@ -39,24 +52,23 @@ assert_equal s3.count_trains_by_type("b"), 1
 
 r1 = Route.new(s3, s1)
 r1.add_station(s2)
-assert_equal r1.get_list_stations.length, 3
+assert_equal r1.list_stations.length, 3
 
-assert_equal t1.get_station, s1
-t1.post_route(r1)
-assert_equal t1.get_station, s3
+assert_equal t1.station, s1
+t1.add_route(r1)
+assert_equal t1.station, s3
 assert_equal s1.count_trains_by_type("a"), 0
 assert_equal s3.count_trains_by_type("a"), 1
 
-
 t1.go_forward
-assert_equal t1.get_station, s2
+assert_equal t1.station, s2
 assert_equal s2.count_trains_by_type("a"), 2
 assert_equal s3.count_trains_by_type("a"), 0
 
 t1.go_forward
-assert_equal t1.get_station, s1
+assert_equal t1.station, s1
 assert_equal s2.count_trains_by_type("a"), 1
 assert_equal s1.count_trains_by_type("a"), 1
 
 t1.go_forward
-assert_equal t1.get_station, s1
+assert_equal t1.station, s1
